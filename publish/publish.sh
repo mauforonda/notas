ae63d4c2abf969d8ddaa90d359a2c758d56ddeac
Mauricio Foronda <josemauricioforonda@gmail.com>
2021-11-04 23:17:51 -0400

    cartera de creditos

--- publish/publish.sh
+++ publish/publish.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-
 filename=$(echo "$1" | sed -e "s/ipynb/html/g")
 jupyter nbconvert --to html "$1"  --TagRemovePreprocessor.remove_all_outputs_tags='{"no_output"}' --TagRemovePreprocessor.remove_cell_tags='{"no_display"}' --no-prompt --template "publish" --stdout > "docs/$filename"
