7f5f0ebac476cafb13234d11adab247be8bdca8c
Mauricio Foronda <josemauricioforonda@gmail.com>
2021-11-05 13:19:17 -0400

    index

---
+++ publish/index.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+index='index.json'
+base='https://mauforonda.github.io/notas/'
+i=0
+n=$(ls *.ipynb | wc -l)
+
+echo '[' > $index
+
+for ipynb in $(ls -t *.ipynb); do
+    
+    let i++
+    filename=$(echo $ipynb | sed -E 's/ipynb/html/g')
+    path=$(echo ${base}${filename})
+    title=$(jq '[.cells[]|select( .cell_type | contains("markdown"))]|first.source[0]' $ipynb | sed -E 's/# //')
+    last_modified=$(date -r $ipynb "+%Y-%m-%d")
+    lines_code=$(jq '.cells[]|select( .cell_type | contains("code"))|.source|length' $ipynb | paste -sd+ | bc)
+    lines_md=$(jq '.cells[]|select( .cell_type | contains("markdown"))|.source|length' $ipynb | paste -sd+ | bc)
+    plots=$(jq '[[.cells[]|select(.cell_type == "code" and .outputs != [])][]|.outputs[]|select(.data|has("image/png"))]|length' $ipynb)
+
+    entry=$(echo "{'path':'$path', 'title':$title, 'last_modified':'$last_modified', 'lines_code':$lines_code, 'lines_md':$lines_md, 'plots':$plots}" | sed -E "s/'/\"/g")
+
+    if [ $i != $n ]; then
+	entry=$(echo "$entry,")
+    fi
+
+    echo $entry >> $index
+    
+done
+
+echo "]" >> $index
